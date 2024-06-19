5293d328afe4517d4b16f1e2a500500d28271d0b
akdrh3 <im7738@naver.com>
2024-06-18 23:41:33 -0400

    number gen input take

--- numberGenerate.py
+++ numberGenerate.py
@@ -1,33 +1,36 @@
 import random as r
 
-f = open("sixfourMillionNum.txt", "w")
-for i in range(0,64000000):
+input_a = int(input())
+
+
+f = open("numbers.txt", "w")
+for i in range(0,input_a * 1000000):
     num = r.randint(0,100000000)
     f.write("%d\n"%num)
 
-f = open("oteMillionNum.txt", "w")
-for i in range(0,128000000):
-    num = r.randint(0,100000000)
-    f.write("%d\n"%num)
+# f = open("oteMillionNum.txt", "w")
+# for i in range(0,128000000):
+#     num = r.randint(0,100000000)
+#     f.write("%d\n"%num)
 
 
-f = open("tfsMillionNum.txt", "w")
-for i in range(0,256000000):
-    num = r.randint(0,100000000)
-    f.write("%d\n"%num)
+# f = open("tfsMillionNum.txt", "w")
+# for i in range(0,256000000):
+#     num = r.randint(0,100000000)
+#     f.write("%d\n"%num)
 
 
-f = open("fotMillionNum.txt", "w")
-for i in range(0,512000000):
-    num = r.randint(0,100000000)
-    f.write("%d\n"%num)
+# f = open("fotMillionNum.txt", "w")
+# for i in range(0,512000000):
+#     num = r.randint(0,100000000)
+#     f.write("%d\n"%num)
 
-f = open("oztfMillionNum.txt", "w")
-for i in range(0,1024000000):
-    num = r.randint(0,100000000)
-    f.write("%d\n"%num)
+# f = open("oztfMillionNum.txt", "w")
+# for i in range(0,1024000000):
+#     num = r.randint(0,100000000)
+#     f.write("%d\n"%num)
 
-f = open("tzfeMillionNum.txt", "w")
-for i in range(0,2048000000):
-    num = r.randint(0,100000000)
-    f.write("%d\n"%num)
+# f = open("tzfeMillionNum.txt", "w")
+# for i in range(0,2048000000):
+#     num = r.randint(0,100000000)
+#     f.write("%d\n"%num)
