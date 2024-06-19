da765df9fbfccf62ac3d620daf41e096e79bd52e
akdrh3 <im7738@naver.com>
2024-06-19 00:25:30 -0400

    numbers

--- mergeSort2.cu
+++ mergeSort2.cu
@@ -55,7 +55,7 @@ void mergeSortHost(int *d_arr, int *d_temp, int left, int right)
 
 int main()
 {
-    std::ifstream inputFile("sixfourMillionNum.txt");
+    std::ifstream inputFile("numbers.txt");
     if (!inputFile)
     {
         std::cerr << "Failed to open the file." << std::endl;
