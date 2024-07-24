83bd9f979c434a539ac6454156b5e7fc0381b195
akdrh3 <im7738@naver.com>
2024-07-24 01:08:47 -0400

    gpu_util header change

--- gpu_util.cuh
+++ gpu_util.cuh
@@ -5,6 +5,6 @@
 
 #define HANDLE_ERROR(err) (HandleError(err, __FILE__, __LINE__))
 
-__host__ void HandleError(cudaError_t err);
+__host__ void HandleError(cudaError_t err, const char *file, int line);
 __host__ void cuda_timer_start(cudaEvent_t *start, cudaEvent_t *stop);
 __host__ float cuda_timer_stop(cudaEvent_t start, cudaEvent_t stop);
