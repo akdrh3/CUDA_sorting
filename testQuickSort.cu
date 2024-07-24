26fdb396b801368799598b1496c2bf80b0a12b73
akdrh3 <im7738@naver.com>
2024-07-23 23:58:51 -0400

    util added

--- testQuickSort.cu
+++ testQuickSort.cu
@@ -1,45 +1,8 @@
 #include "gpu_util.cuh"
 #include <cuda_runtime.h>
 #include <stdio.h>
-
-void read_from_file(char *file_name, int **numbers, uint64_t size_of_array) {
-    FILE *file = fopen(file_name, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
-
-    *numbers = (int *)malloc(size_of_array * sizeof(int));
-    if (*numbers == NULL) {
-        perror("Error allocating memory");
-        exit(EXIT_FAILURE);
-    }
-
-    for (uint64_t i = 0; i < size_of_array; i++) {
-        if (fscanf(file, "%d", &(*numbers)[i]) == EOF) {
-            perror("Error reading from file");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    fclose(file);
-}
-
-uint64_t count_size_of_file(char *file_name) {
-    FILE *file = fopen(file_name, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
-
-    uint64_t count = 0;
-    int tmp = 0;
-    while (fscanf(file, "%d", &tmp) != EOF) {
-        count++;
-    }
-
-    fclose(file);
-    return count;
+extern "C" {
+#include "util.h"
 }
 
 int main() {
@@ -48,7 +11,7 @@ int main() {
     scanf("%255s", file_name);
 
     uint64_t size_of_array = count_size_of_file(file_name);
-    printf("Number of integers in the file : %llu\n", size_of_array);
+    printf("Number of integers in the file : %lu\n", size_of_array);
 
     int *number_array = NULL;
     read_from_file(file_name, &number_array, size_of_array);
---
+++ util.h
@@ -0,0 +1,12 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_from_file(char *file_name, int **numbers, uint64_t size_of_array);
+
+uint64_t count_size_of_file(char *file_name);
+
+#endif
