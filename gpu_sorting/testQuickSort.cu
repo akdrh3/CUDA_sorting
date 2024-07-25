a7ee8b3b16b1d599178c4d3eb56966ef8c04bad0
akdrh3 <im7738@naver.com>
2024-07-24 23:14:11 -0400

    printing array

--- gpu_sorting/testQuickSort.cu
+++ gpu_sorting/testQuickSort.cu
@@ -26,6 +26,8 @@ __device__ int64_t partition(int *arr, int64_t low, int64_t high) {
     }
     printf("last swapping %d and %d\n", arr[i + 1], arr[high]);
     swap(&arr[i + 1], &arr[high]);
+    printf("current array\n");
+    print_array(arr, high);
 
     return i + 1;
 }
