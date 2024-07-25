97cf760ad59c7bad567e58a84c230f418871b93c
akdrh3 <im7738@naver.com>
2024-07-25 01:48:40 -0400

    sh

--- gpu_sorting/run_tests.sh
+++ gpu_sorting/run_tests.sh
@@ -2,7 +2,7 @@
 # Save this as runAllTests.sh
 
 # Array of numbers
-numbers=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
+numbers=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384)
 
 # Iterate over each number in the array
 for number in "${numbers[@]}"
@@ -12,9 +12,10 @@ do
     echo $number | python3 numberGenerate.py
 
     # Run testQuickSort with numbers.txt and capture output
-    echo "numbers.txt" | ./testQuickSort >> output.txt
+    output=$(echo "numbers.txt" | ./testQuickSort 2>> error_log.txt)
 
     # Print and save output
+    echo "$output"
     echo "$output" >> output.txt
 
     # Separate outputs with a line
