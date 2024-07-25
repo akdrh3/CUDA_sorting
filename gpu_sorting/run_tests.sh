9abd9194357fed250f180c0377580ce07b8ad10f
akdrh3 <im7738@naver.com>
2024-07-25 02:03:26 -0400

    max increased

--- gpu_sorting/run_tests.sh
+++ gpu_sorting/run_tests.sh
@@ -2,7 +2,7 @@
 # Save this as runAllTests.sh
 
 # Array of numbers
-numbers=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384)
+numbers=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304)
 
 # Iterate over each number in the array
 for number in "${numbers[@]}"
