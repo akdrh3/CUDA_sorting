e2a6019e9c53f31bc339bb8b7e40acc48bec7e2b
akdrh3 <im7738@naver.com>
2024-07-31 02:06:13 -0400

    sdfas

--- gpu_sorting/merge_tests.sh
+++ gpu_sorting/merge_tests.sh
@@ -5,7 +5,7 @@
 numbers=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304)
 
 # Clear the output file at the beginning
-# > mergeoutput.txt
+> merge_error_log.txt
 
 # Iterate over each number in the array
 for number in "${numbers[@]}"
